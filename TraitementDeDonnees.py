f69de55e4f63311d371b1dca97d83dfa59037899
cyliaL <hc_laoufi@esi.dz>
2022-04-07 02:58:57 +0100

    third commit

--- TraitementDeDonnees.py
+++ TraitementDeDonnees.py
@@ -1,3 +1,13 @@
-indice=10   
-for i in indice:
-    print(i,", ")
+
+
+import Transaction
+
+class TraitementDeDonnees:
+   def __init__(self):
+        self.time=0
+        self.nbTransactions=0
+        self.nbItems=0
+        self.totalItems=0
+        self.bdd=[Transaction()]
+
+ 
