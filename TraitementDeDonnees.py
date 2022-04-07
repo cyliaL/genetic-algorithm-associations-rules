5d8ea4fbca524c6bcd9c46398fba56106780ee4c
cyliaL <hc_laoufi@esi.dz>
2022-04-07 15:04:56 +0100

    six commit

---
+++ TraitementDeDonnees.py
@@ -0,0 +1,21 @@
+
+
+class TraitementDeDonnees:
+   def __init__(self):
+        self.time=0
+        self.nbTransactions=0
+        self.nbItems=0
+        self.totalItems=0
+        self.bdd=[]
+
+
+
+bdd=[]
+
+bdd.append(["1","2"])
+
+bdd.append(["D"])
+
+
+
+print(bdd[0][0])
