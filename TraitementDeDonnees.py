bb9a4766932b735ef03f7fa5f26096913b0300d5
cyliaL <hc_laoufi@esi.dz>
2022-04-07 13:29:23 +0100

    fifth commit

--- TraitementDeDonnees.py
+++
@@ -1,9 +0,0 @@
-import Transaction
-
-class TraitementDeDonnees:
-   def __init__(self):
-        self.time=0
-        self.nbTransactions=0
-        self.nbItems=0
-        self.totalItems=0
-        self.bdd=[Transaction()]
