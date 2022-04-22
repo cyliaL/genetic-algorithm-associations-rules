2630abf8536c39fe84a6b0932945ceb29a7a580c
cyliaL <hc_laoufi@esi.dz>
2022-04-22 15:51:32 +0100

    correct version27

--- TraitementDeDonnees.py
+++ TraitementDeDonnees.py
@@ -3,8 +3,18 @@ import time
 from datetime import datetime
 from ExempleBDD import ExempleBDD
 import pandas as pd
+import csv
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn import svm
+from sklearn.tree import DecisionTreeRegressor 
+from sklearn.neural_network import MLPRegressor
+
+
 class TraitementDeDonnees:
     time=0
+    temps_encod=0
     nbTransactions=0
     nbItems=0
     totalItems=[]
@@ -88,9 +98,81 @@ class TraitementDeDonnees:
         TraitementDeDonnees.incTime(elapsedTime)
         return cout((alpha*support+beta*confiance)/(alpha+beta),support,confiance)
 
+    #temps de calcul de la la fitness durant l'optimisation
     @staticmethod
     def incTime(ti):
         TraitementDeDonnees.time += ti
+
+
+    @staticmethod
+    def saveDonnees(totalData):
+        l=TraitementDeDonnees.totalItems+['fitness']
+        with open("./results/results_AG_simple.csv", 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(l)
+            for r in totalData:
+                debut_encod = time.time()
+                r.encoder()
+                fin_encod = time.time()
+                TraitementDeDonnees.temps_encod += (fin_encod - debut_encod) 
+                writer.writerow(r.binary)    
+
+    @staticmethod
+    def generateModelHorsLigne(numModel):
+        dataset = pd.read_csv('results/results_AG_simple.csv')
+        co = TraitementDeDonnees.totalItems
+        one_hot_encoded_data = pd.get_dummies(dataset, columns = co)
+        limit_sup = TraitementDeDonnees.nbItems * 3 +1
+        x= one_hot_encoded_data.iloc[:,1:limit_sup].values #les colones de 1 limit_sup
+        y= one_hot_encoded_data.iloc[:,0].values # la colonne fitnesse 0
+        x_train, x_test, y_train, y_test =train_test_split(x, y,test_size=0.25, random_state=0)
+        '''--------------------------------------------------------------------------------'''
+        if(numModel==0):
+            print("Random Forest Regressor")
+            temps_debut = time.time()
+            TraitementDeDonnees.model = RandomForestRegressor(n_estimators = 30, random_state = 0)
+            TraitementDeDonnees.model.fit(x_train,y_train)
+            temps_fin = time.time()
+            y_pred_RF = TraitementDeDonnees.model.predict(x_test)
+            print('temps d''entrainement = ',temps_fin-temps_debut)
+            print('Erreur quadratique moyenne (Root Mean Squared Error):', mean_squared_error(y_test, y_pred_RF))
+        print("--------------------------------------------------------------------------------")
+        if(numModel==1):
+            print("Support Vector Regressor")
+            temps_debut = time.time()
+            TraitementDeDonnees.model = svm.SVR()
+            TraitementDeDonnees.model.fit(x_train,y_train)
+            temps_fin = time.time()
+            y_pred_SVM=TraitementDeDonnees.model.predict(x_test)
+            print('temps d''entrainement = ',temps_fin-temps_debut)
+            print('Erreur quadratique moyenne (Root Mean Squared Error):', mean_squared_error(y_test, y_pred_SVM))
+        print("--------------------------------------------------------------------------------")
+        if(numModel==2):
+            print("Decison Tree Regressor")
+            temps_debut = time.time()
+            TraitementDeDonnees.model = DecisionTreeRegressor(random_state = 0)
+            TraitementDeDonnees.model.fit(x_train,y_train)
+            temps_fin = time.time()
+            y_pred_DT=TraitementDeDonnees.model.predict(x_test)
+            print('temps d''entrainement = ',temps_fin-temps_debut)
+            print('Erreur quadratique moyenne (Root Mean Squared Error):', mean_squared_error(y_test, y_pred_DT))
+        print("--------------------------------------------------------------------------------")
+        if(numModel==3):
+            print("Neural Network Regressor")
+            temps_debut = time.time()
+            TraitementDeDonnees.model = MLPRegressor(max_iter=2000)
+            TraitementDeDonnees.model.fit(x_train,y_train)
+            temps_fin = time.time()
+            y_pred_NN=TraitementDeDonnees.model.predict(x_test)
+            print('temps d''entrainement = ',temps_fin-temps_debut)
+            print('Erreur quadratique moyenne (Root Mean Squared Error):', mean_squared_error(y_test, y_pred_NN))
+
+    @staticmethod
+    def calculFitnessModelHorsLigne(population):
+        return TraitementDeDonnees.model.predict(population)
+
+        
+
 
 #TraitementDeDonnees.lireDonneesBinaires("data\DataSet1.txt")
 #print(TraitementDeDonnees.bdd)
--- test.py
+++ test.py
@@ -1,77 +1,13 @@
-import math
-import csv
-from xmlrpc.client import boolean
-from cv2 import sort
-import time
-from numpy import append
-from regex import P
-from Chromosome import Chromosome
-import random
 from Cout import cout
 from TraitementDeDonnees import TraitementDeDonnees
+from AGetMLHorsLigne import AGetMLHorsLigne
 from AG import AG
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from sklearn import svm
-from sklearn.tree import DecisionTreeRegressor 
-from sklearn.neural_network import MLPRegressor
-
 
 TraitementDeDonnees.lireDonneesBinaires("data\DataSet1.txt")
-ag=AG(100,100,0.4,0.6,0.5,0.5,5,0.3,0.6,True,0,0,0)
-ag.lancerAlgoGen()
-ag.saveDonnees()
-print("le temps d'encodage des chromosomes = ",ag.temps_encod)
-print("le temps d'execution de l'AG séquentiel = ",ag.temps_exec)
-print("--------------------------------------------------------------------------------")
-
-'''--------------------------------------------------------------------------------'''
-# pré-traitement
-
-dataset = pd.read_csv('results/results_AG_simple.csv')
-co = TraitementDeDonnees.totalItems
-one_hot_encoded_data = pd.get_dummies(dataset, columns = co)
-limit_sup = TraitementDeDonnees.nbItems * 3 +1
-x= one_hot_encoded_data.iloc[:,1:limit_sup].values #les colones de 1 limit_sup
-y= one_hot_encoded_data.iloc[:,0].values # la colonne fitnesse 0
-x_train, x_test, y_train, y_test =train_test_split(x, y,test_size=0.25, random_state=0)
-'''--------------------------------------------------------------------------------'''
-print("Random Forest Regressor")
-temps_debut = time.time()
-regressor_RF = RandomForestRegressor(n_estimators = 30, random_state = 0)
-regressor_RF.fit(x_train,y_train)
-temps_fin = time.time()
-y_pred_RF = regressor_RF.predict(x_test)
-print('temps d''entrainement = ',temps_fin-temps_debut)
-print('Erreur quadratique moyenne (Root Mean Squared Error):', mean_squared_error(y_test, y_pred_RF))
-print("--------------------------------------------------------------------------------")
-print("Support Vector Regressor")
-temps_debut = time.time()
-regressor_SVM = svm.SVR()
-regressor_SVM.fit(x_train,y_train)
-temps_fin = time.time()
-y_pred_SVM=regressor_SVM.predict(x_test)
-print('temps d''entrainement = ',temps_fin-temps_debut)
-print('Erreur quadratique moyenne (Root Mean Squared Error):', mean_squared_error(y_test, y_pred_SVM))
-print("--------------------------------------------------------------------------------")
-print("Decison Tree Regressor")
-temps_debut = time.time()
-regressor_DT = DecisionTreeRegressor(random_state = 0)
-regressor_DT.fit(x_train,y_train)
-temps_fin = time.time()
-y_pred_DT=regressor_DT.predict(x_test)
-print('temps d''entrainement = ',temps_fin-temps_debut)
-print('Erreur quadratique moyenne (Root Mean Squared Error):', mean_squared_error(y_test, y_pred_DT))
-
-print("--------------------------------------------------------------------------------")
-print("Neural Network Regressor")
-temps_debut = time.time()
-regressor_NN = MLPRegressor(max_iter=2000).fit(x_train,y_train)
-regressor_NN.fit(x_train,y_train)
-temps_fin = time.time()
-y_pred_NN=regressor_NN.predict(x_test)
-print('temps d''entrainement = ',temps_fin-temps_debut)
-print('Erreur quadratique moyenne (Root Mean Squared Error):', mean_squared_error(y_test, y_pred_NN))
+#ag=AG(100,100,0.4,0.6,0.5,0.5,5,0.3,0.6,True,0,0,0)
+#ag.lancerAlgoGen()
+TraitementDeDonnees.generateModelHorsLigne(0)
+agHorsLigne=AGetMLHorsLigne(100,100,0.4,0.6,0.5,0.5,5,0.3,0.6,True,0,0,0)
+agHorsLigne.lancerAlgoGen()
