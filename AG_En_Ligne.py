9b7edca2392f18ddc24d11d221a992b580e842a6
cyliaL <hc_laoufi@esi.dz>
2022-04-29 18:15:08 +0100

    correct version30

---
+++ AG_En_Ligne.py
@@ -0,0 +1,426 @@
+import math
+import csv
+from xmlrpc.client import boolean
+from cv2 import sort
+import time
+from numpy import append
+from regex import P
+from Chromosome import Chromosome
+import random
+import copy
+from Cout import cout
+from TraitementDeDonnees import TraitementDeDonnees
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn import svm
+from sklearn.tree import DecisionTreeRegressor 
+from sklearn.neural_network import MLPRegressor
+
+
+class AG_En_Ligne:
+
+    def __init__(self, nbIteration, taillePop, alpha, beta, pc, pm,  tailleMaxChromosome, minsup, minconf, 
+    michigan ,typeCroisement, typeRemplacement, typeExec,typeModel):
+        self.nbIterations=nbIteration
+        self.taillePop=taillePop
+        self.alpha=alpha
+        self.beta=beta
+        self.pc=pc
+        self.pm=pm
+        self.tailleMaxChromosome=tailleMaxChromosome
+        self.minsup=minsup
+        self.minconf=minconf
+        self.michigan=michigan
+        self.typeCroisement=typeCroisement
+        self.typeRemplacement=typeRemplacement
+        self.TypeExec = typeExec
+        self.typeModel=typeModel
+        self.population = []
+
+
+        self.pop_train = []
+        self.pop_train_val = []
+
+        self.temps_entrainement=0
+        self.temps_prediction=0
+        self.temps_exec = 0
+        self.temps_encod=0
+        
+        for i in range(self.taillePop):
+            while(True):
+                #print("hhh")
+                nouveau=True
+                t=random.randrange(2, self.tailleMaxChromosome+1)
+                c=Chromosome(t,0,0,0,0,self.alpha,self.beta,False)
+                ind=random.randrange(1, t)
+                if(self.michigan):
+                    c.setIndice(t-1)
+                else:
+                    c.setIndice(ind)
+
+                c.chromosomeAlea()
+                #calcul = TraitementDeDonnees.calculFitnessCPU(c,self.alpha,self.beta)
+                #c.setCout(calcul.getFitness())
+                #c.setConfiance(calcul.getConfiance())
+                #c.setSupport(calcul.getSupport())
+
+                for x in self.population:
+                    if x.equals(c):
+                        nouveau=False
+                        break
+
+                if(nouveau==True):
+                    c.encoderBinaire()
+                    self.population.append(c)
+                    self.pop_train.append(c.binary)
+                    break
+
+        
+        self.calculCoutPop()
+        for r in self.population:
+            self.pop_train_val.append(r.getCout())
+
+        self.ind_best=self.population[0]
+        self.inv_condidate=self.population[0]
+
+        '''for i in range(self.taillePop - 1):
+            if self.population[i + 1].getCout() > self.ind_best.getCout():
+                self.ind_best = self.population[i + 1]'''
+
+
+
+        
+         
+
+       
+                
+                            
+
+    def initialize(self):
+        for i in range(self.taillePop):
+            while(True):
+                #print("hhh")
+                nouveau=True
+                t=random.randrange(2, self.tailleMaxChromosome+1)
+                c=Chromosome(t,0,0,0,0,self.alpha,self.beta,False)
+                ind=random.randrange(1, t)
+                if(self.michigan):
+                    c.setIndice(t-1)
+                else:
+                    c.setIndice(ind)
+
+                c.chromosomeAlea()
+                #calcul = TraitementDeDonnees.calculFitnessCPU(c,self.alpha,self.beta)
+                #c.setCout(calcul.getFitness())
+                #c.setConfiance(calcul.getConfiance())
+                #c.setSupport(calcul.getSupport())
+
+                for x in self.population:
+                    if x.equals(c):
+                        nouveau=False
+                        break
+
+                if(nouveau==True):
+                    c.encoderBinaire()
+                    self.population.append(c)
+                    self.pop_train.append(c.binary)
+                    break
+
+        
+        self.calculCoutPop()
+        for r in self.population:
+            self.pop_train_val.append(r.getCout())
+
+        self.ind_best=self.population[0]
+
+        for i in range(self.taillePop - 1):
+            if self.population[i + 1].getCout() > self.ind_best.getCout():
+                self.ind_best = self.population[i + 1]
+
+
+    def train_model(self):
+        if(self.typeModel==0):
+            print("Random Forest Regressor")
+            temps_debut = time.time()
+            self.model = RandomForestRegressor(n_estimators = 30, random_state = 0)
+            self.model.fit(self.pop_train,self.pop_train_val)
+            temps_fin = time.time()
+            self.temps_entrainement=temps_debut-temps_fin
+            #print('Erreur quadratique moyenne (Root Mean Squared Error):', mean_squared_error(y_test, y_pred_RF))
+        
+        if(self.typeModel==1):
+            print("Support Vector Regressor")
+            temps_debut = time.time()
+            self.model = svm.SVR()
+            self.model.fit(self.pop_train,self.pop_train_val)
+            temps_fin = time.time()
+            self.temps_entrainement=temps_fin-temps_debut
+            #print('Erreur quadratique moyenne (Root Mean Squared Error):', mean_squared_error(y_test, y_pred_SVM))
+        if(self.typeModel==2):
+            print("Decison Tree Regressor")
+            temps_debut = time.time()
+            self.model = DecisionTreeRegressor(random_state = 0)
+            self.model.fit(self.pop_train,self.pop_train_val)
+            temps_fin = time.time()
+            self.temps_entrainement=temps_fin-temps_debut
+            #print('Erreur quadratique moyenne (Root Mean Squared Error):', mean_squared_error(y_test, y_pred_DT))
+        if(self.typeModel==3):
+            print("Neural Network Regressor")
+            temps_debut = time.time()
+            self.model = MLPRegressor(max_iter=2000)
+            self.model.fit(self.pop_train,self.pop_train_val)
+            temps_fin = time.time()
+            self.temps_entrainement=temps_fin-temps_debut
+
+
+    
+    def model_predict(self,x):
+        val = self.rfr_val.predict(x)
+        return val
+    
+
+    def update_model(self):
+        pop_sort=self.trierPop()
+        for i in range(len(pop_sort)):
+            self.ind_candidate = pop_sort[i]
+            if self.ind_candidate not in (self.pop_train):
+                #print(len(self.pop_train))
+                #print(len(self.pop_train_val))
+                self.ind_candidate.encoderBinaire()
+                self.eval_function(self.ind_candidate)
+                self.pop_train.append(self.ind_candidate.binary)
+                self.pop_train_val.append(self.ind_candidate.getCout())
+                self.model.fit(self.pop_train, self.pop_train_val)
+                break
+
+    def update_best(self):
+        if self.ind_candidate.getCout() > self.ind_best.getCout():
+            self.ind_best = self.ind_candidate
+
+        
+    def eval_function(self,c):
+        if(self.TypeExec==0):
+            c.calculerCoutRegleCPU()
+
+
+    def calculCoutPop(self):
+        if(self.TypeExec==0) : 
+            for r in self.population:
+                r.calculerCoutRegleCPU()
+                #calcul=TraitementDeDonnees.calculFitnessCPU(r,self.alpha,self.beta)
+                #r.setCout(calcul.getFitness())
+                #r.setConfiance(calcul.getConfiance())
+                #r.setSupport(calcul.getSupport())
+
+        #elif(self.TypeExec==2) : self.population[i].calculerCoutRegleGPUDist()
+        #elif(self.TypeExec==4) : self.population[i].calculerCoutReglesurThreads()
+
+    def lancerAlgoGen(self):
+        debut_exec = time.time()
+
+        self.train_model()
+        if(self.TypeExec==0):  
+            #self.afficherPop() # population initiale
+            for i in range(self.nbIterations):
+                self.croisement()
+                self.mutation()
+                self.update_model()
+                self.update_best()
+        self.afficherPop()
+        fin_exec = time.time()
+        self.temps_exec += (fin_exec - debut_exec)
+        print("le temps d'execution de l'AG simple = ",self.temps_exec)
+        TraitementDeDonnees.saveDonneesBinaires(self.totalData)
+        #self.AfficherReglesValide()
+        #self.stats()
+
+
+
+    def croisement(self): 
+        #self.trierPop()
+        #======> selection des paires
+        paires=[]
+        indice=0
+        utilisees=set()
+        for j in range(self.taillePop):
+            while(True):
+                rand=random.random()
+                indice+=1
+                if(indice==self.taillePop):
+                    indice=0
+                while (indice in utilisees):
+                    indice+=1
+                    if indice==self.taillePop:
+                        indice=0
+                if(rand<=0.5):
+                    break
+            paires.append(indice)
+            utilisees.add(indice)
+        if(len(paires)%2==1):
+            fin=len(paires)-1
+        else:
+            fin=len(paires)
+        #======> croisement
+        for j in range(0, fin-1,2):
+            if(random.random()<self.pc):
+                ef1=Chromosome(self.population[paires[j]].getTaille(),0,0,0,self.population[paires[j]].getIndice(),self.alpha,self.beta,False)
+                ef2=Chromosome(self.population[paires[j+1]].getTaille(),0,0,0,self.population[paires[j+1]].getIndice(),self.alpha,self.beta,False)
+                minimum=min(ef1.getTaille(),ef2.getTaille())
+                #======> croisement à un point
+                if self.typeCroisement==0:
+                    #======> recopier la premiere partie des parents vers les fils
+                    point=random.randrange(1,minimum)
+                    for k in range(point):
+                        ef1.getItems().append(self.population[paires[j]].getItems()[k])
+                        ef2.getItems().append(self.population[paires[j+1]].getItems()[k])
+                     #======> echanger la deuxième partie das parents vers les fils
+                    for k in range(point,minimum):
+                        if (not self.population[paires[j]].contient(self.population[paires[j+1]].getItems()[k])) and (not self.population[paires[j+1]].contient(self.population[paires[j]].getItems()[k])):
+                            ef1.getItems().append(self.population[paires[j+1]].getItems()[k])
+                            ef2.getItems().append(self.population[paires[j]].getItems()[k])
+                        else:
+                            ef1.getItems() .append(self.population[paires[j]].getItems()[k])
+                            ef2.getItems().append(self.population[paires[j+1]].getItems()[k])
+                    #======> copier les items restants de la regle 1
+                    for k in range(minimum,ef1.getTaille(),1):
+                        ef1.getItems().append(self.population[paires[j]].getItems()[k])   
+                    #======> copier les items restants de la regle 2
+                    for k in range(minimum,ef2.getTaille(),1):
+                        ef2.getItems().append(self.population[paires[j+1]].getItems()[k])   
+                    #======> evaluation des individus fils
+                    if self.TypeExec==0:
+                        c1=TraitementDeDonnees.calculFitnessCPU(ef1,self.alpha,self.beta)
+                        c2=TraitementDeDonnees.calculFitnessCPU(ef2,self.alpha,self.beta)
+                        ef1.setCout(c1.getFitness())
+                        ef1.setSupport(c1.getSupport())
+                        ef1.setConfiance(c1.getConfiance())
+                        ef2.setCout(c2.getFitness())
+                        ef2.setConfiance(c2.getConfiance())
+                        ef2.setSupport(c2.getSupport())
+
+                    
+                    #======> remplacement des parents
+                    remplace=0
+                    #======> remplacement de la regle 1
+                    if (not self.contientRegle(ef1)):
+                        if ef1.getCout()> self.population[paires[j]].getCout():
+                            self.population[paires[j]]= ef1
+                            remplace=1
+                        else: 
+                            if ef1.getCout()> self.population[paires[j+1]].getCout():
+                                self.population[paires[j+1]]= ef1
+                                remplace=2
+                    #======> remplacement de la regle 2 
+                    if not self.contientRegle(ef2):
+                        if remplace ==0  and ef2.cout> self.population[paires[j]].getCout():
+                            self.population[paires[j]]= ef2
+                        else: 
+                            if remplace ==0  and ef2.cout> self.population[paires[j+1]].getCout():
+                                self.population[paires[j+1]]= ef2
+                            else: 
+                                if remplace ==1  and ef2.cout> self.population[paires[j+1]].getCout():
+                                    self.population[paires[j+1]]= ef2
+                                else: 
+                                    if remplace ==2  and ef2.cout> self.population[paires[j]].getCout():
+                                        self.population[paires[j]]= ef2
+
+
+    def contientRegle(self,c):
+        for i in range(self.taillePop):
+            if self.population[i].equals(c):
+                return True
+        return False
+        
+
+    def afficherPop(self):
+        for x in self.population:
+            x.afficherRegle()
+
+
+    def AfficherReglesValide(self):
+        print("----------Les Regles valides----------")
+        for j in range(self.taillePop):
+            if( self.population[j].getSupport() > self.minsup and self.population[j].getConfiance() > self.minconf):
+                self.population[j].setValide(True)
+                self.population[j].afficherRegle()
+            else : self.population[j].setValide(False)
+
+    def stats(self):
+        countRegles=0
+        countItems=0
+        moyenne=0
+        moyConf=0
+        moySupp=0
+        moyTaille=0
+        items= []
+        for c in self.population:
+            if c.valide:
+                countRegles += 1
+                moyTaille += c.getTaille()
+                moyenne += c.getCout()
+                moyConf+=c.getConfiance()
+                moySupp+= c.getSupport()
+                for k in range(c.getTaille()):
+                    if c.getItems()[k] not in items:
+                        items.append(c.getItems()[k])
+                        countItems += 1  
+        if(countRegles!=0): 
+            moyTaille/= countRegles
+            moyenne/= countRegles
+            moySupp/= countRegles
+            moyConf/= countRegles
+            print("Le nombre de régles est : " ,countRegles)
+            print("Le cout moyen est : " ,moyenne)
+            print("Le support moyen est : " ,moySupp)
+            print("La confiance moyenne est : " ,moyConf)
+            print("Le nombre d'items utilisés : " ,countItems)
+            print(" la taille moyenne est de : ",moyTaille)
+            print("le temps d'execution = ",self.temps_exec, "secondes")
+            print("le temps d'encodage = ",self.temps_encod, "secondes")
+            
+
+
+
+    #petit probleme de constructeur dans Chromosome a régler aprés on peut mettre les valeur de init == o
+    def trierPop(self):
+        pop_sort = copy.deepcopy(self.population)
+        for i in range(1,self.taillePop):
+            for j in range(self.taillePop-i):
+                if( pop_sort[j].getCout()>pop_sort[j+1].getCout()):
+                    save = pop_sort[j]
+                    pop_sort[j]=pop_sort[j+1]
+                    pop_sort[j+1]=save
+        return pop_sort
+		
+
+    def mutation(self):
+        for j in range(self.taillePop):
+            d=random.uniform(0, 1)
+            if(d<self.pm):
+                while True:
+                    #print("gggg")
+                    mut = Chromosome(self.population[j].getTaille(),self.population[j].getCout(),self.population[j].getSupport(),self.population[j].getConfiance(),self.population[j].getIndice(),self.population[j].getAlpha(),self.population[j].getBeta(),self.population[j].getValide())
+                    for k in range(mut.getTaille()):
+                        mut.getItems().append(self.population[j].getItems()[k])
+                    #mut.items = self.population[j].getItems()
+                    indice = random.randint(0,mut.getTaille()-1)
+                    while True:
+                        val = TraitementDeDonnees.totalItems[random.randrange(0, TraitementDeDonnees.nbItems,1)]
+                        if(not mut.contient(val)):
+                            break
+                    mut.getItems()[indice] = val
+                    if(not self.contientRegle(mut)):
+                        break
+                self.population[j]=mut
+                c=TraitementDeDonnees.calculFitnessCPU(self.population[j],self.alpha,self.beta)
+                self.population[j].setCout(c.getFitness())
+                self.population[j].setSupport(c.getSupport())
+                self.population[j].setConfiance(c.getConfiance())
+    
+
+TraitementDeDonnees.lireDonneesSynthetiques('data\DataSet5.txt')
+ag=AG_En_Ligne(100,30,0.4,0.6,0.5,0.5,3,0.3,0.6,True,0,0,0,0)
+
+ag.lancerAlgoGen()
--- test.py
+++ test.py
@@ -8,12 +8,15 @@ from AG_Hors_Ligne import AG_Hors_Ligne
 
 TraitementDeDonnees.lireDonneesSynthetiques('data\DataSet5.txt')
 print("hello")
+TraitementDeDonnees.generateModelHorsLigneBinaire(0)
+val=TraitementDeDonnees.model.predict([[0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0]])
+print(val)
 #print(TraitementDeDonnees.totalItems)
 #print(TraitementDeDonnees.nbItems)
 #print(TraitementDeDonnees.nbTransactions)
 #print(TraitementDeDonnees.bdd)
 #TraitementDeDonnees.lireDonneesBinaires("data\data_test.txt")
-ag=AG(100,30,0.4,0.6,0.5,0.5,3,0.3,0.6,True,0,0,0)
+'''ag=AG(100,30,0.4,0.6,0.5,0.5,3,0.3,0.6,True,0,0,0)
 ag.lancerAlgoGen()
 print("le temps de calcul de la fitness réelle durant l'optimisation = ", TraitementDeDonnees.temps_calc_fitness)
 print("le temps de l'encodage = ", TraitementDeDonnees.temps_encod)
@@ -29,7 +32,7 @@ print("le temps de prédiction = ", TraitementDeDonnees.temps_prediction)
 print("le temps d'encodage = ", TraitementDeDonnees.temps_encod)
 
 
-'''
+
 print("==========================================================================================")
 TraitementDeDonnees.temps_calc_fitness=0
 TraitementDeDonnees.temps_encod=0
